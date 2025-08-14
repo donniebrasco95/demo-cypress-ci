df16005b1bbfaf1b4f86203d827aa6b3a43620f7
donniebrasco95 <matthieuboise@outlook.fr>
2025-08-14 03:19:02 +0200

    Ajout Cypress API dans test-homepage (#1)
    * Ajout Cypress API dans test-homepage
    
    * Lancer CI en cas de pull request
    
    ---------
    
    Co-authored-by: Matthieu Boise <matthieuboise@MacBook-Pro-de-Matthieu.local>

--- cypress/e2e/test-homepage.cy.js
+++ cypress/e2e/test-homepage.cy.js
@@ -2,7 +2,9 @@
 
 describe('Page d’accueil', () => {
   it('devrait afficher le titre correct', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('Kitchen Sink')
+    cy.visit('https://example.cypress.io');
+    cy.contains('Kitchen Sink');
+    cy.contains('Cypress API').click();
+    cy.url().should('include', '/cypress-api');
   })
 })
